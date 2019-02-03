17631b44aef30f5171f856851ed33b3708fcf0c3
Adrienne <adrienneeaston@gmail.com>
2019-02-03 13:48:10 -0800

    complete with error in question 6, reads non-number as correct

--- js/app.js
+++ js/app.js
@@ -1,22 +1,28 @@
 
 'use strict';
 
+// This program asks the user 7 questions about me and at teh ends provides feedback as to how many they got correct.
+
 var counter = 0;
+
+// Question 1, yes or no question.
 
 var born = prompt('Pop quiz about me! Was I born in Michigan? Yes or No?');
 console.log('User answered ' + born +', "for the question "Was I born in Michigan"');
-born.toLowerCase();
+born = born.toLowerCase();
 
-if (born === 'yes') {
-  alert('No, actually I\'ve never even been there!');
-} else {
+if (born === 'no') {
   counter++;
   alert('Correct! I was born in Yorba Linda, California.');
+} else {
+  alert('No, actually I\'ve never even been there!');
 }
+
+// Question 2, yes or no question.
 
 var work = prompt('Was my first job working at Disneyland? Yes or No?');
 console.log('User answered ' + work +', "Was my first job working at Disneyland?"');
-born.toLowerCase();
+work = work.toLowerCase();
 
 if (work === 'yes') {
   counter++;
@@ -25,9 +31,11 @@ if (work === 'yes') {
   alert('But is was! I served frozen yogurt, baked goods, and bad coffee on Main Street!');
 }
 
+// Question 3, yes or no question.
+
 var pets = prompt('Do I have cat? Yes or No?');
 console.log('User answered ' + pets +', "Do I have a cat?"');
-born.toLowerCase();
+pets = pets.toLowerCase();
 
 if (pets === 'yes') {
   counter++;
@@ -36,20 +44,24 @@ if (pets === 'yes') {
   alert('Wrong! I do have a cat and she is the fluffiest!');
 }
 
+// Question 4, yes or no question.
+
 var music = prompt('Is Pearl Jam my favorite band? Yes or No?');
 console.log('User answered ' + music +', "Is Pearl Jam my favorite band?"');
-born.toLowerCase();
+music = music.toLowerCase();
 
-if (music === 'yes') {
-  alert('No way! Oingo Boingo forever!');
-} else {
+if (music === 'no') {
   counter++;
   alert('You are correct! It is Oingo Boingo that will forever have my heart.');
+} else {
+  alert('No way! Oingo Boingo forever!');
 }
+
+// Question 5, yes or no question.
 
 var candy = prompt('Is Turkish Delight my favorite candy? Yes or No?');
 console.log('User answered ' + candy +', "Is turkish Delight my favorite candy?"');
-born.toLowerCase();
+candy = candy.toLowerCase();
 
 if (candy === 'yes') {
   counter++;
@@ -59,25 +71,26 @@ if (candy === 'yes') {
 }
 
 
-// This is my 6th question using for loop programming to ask the user 4 times before ending the question.
+// Question 6, Asks the user for a number value, gives them 4 chances, loop ends when tehy have gotten it right or answered 4 times.
+// Error in this question is that it currently views non-numbers as correct answers. When I tried fixing it by ending in the wrong answer for "else", it broke the code, so I switched it back for now.
 
-for (var i = 1; i < 5; i++) {
+for (var i = 0; i < 4; i++) {
 
   var number = prompt('How many McDonald\'s hamburgers have I eaten in my life?');
   console.log('User believes I had ' + number + ' "McDonald\'s hamburgers in my life"');
-
-  // This will ensure that even if the user inputs a negative number, it will not say "too high!".
 
   if (number > 0) {
     alert('Too high!');
   } else if (number < 0) {
     alert('Too low!');
   } else {
-    counter++;
-    alert('Zero is correct! McDonald\'s hamburgers look gross!');
-    break;
+  counter++;
+  alert('Zero is correct! McDonald\'s hamburgers look gross!');
+  break;
   }
 }
+
+// Question 7, asks for an answer with multiple possible correct answers stored in an array. The loop stops after they have either gotten it right or have tried 6 times.
 
 for (var u = 0; u < 6; u++){
 
@@ -88,20 +101,22 @@ for (var u = 0; u < 6; u++){
   ];
 
   var userTvAnswer = prompt('What is one of my favorite current tv shows?');
-  userTvAnswer.toLowerCase();
-  if(televisionShows.includes(userTvAnswer)){
+  userTvAnswer = userTvAnswer.toLowerCase();
+  if(televisionShows.includes(userTvAnswer)) {
     counter++;
-    alert('yay you got it right. My current favorites are, ' + televisionShows[0] + ', ' + televisionShows[1] + ', ' + televisionShows[2]);
+    alert('Good job! You recognize my excellent taste in TV viewing! My current favorites are, ' + televisionShows[0] + ', ' + televisionShows[1] + ', ' + televisionShows[2]);
     console.log('User answered ' + userTvAnswer + ', to the question "What is one of my favorited current tv shows?"');
     break;
   }
   else {
-    alert('Nope!');
+    alert('Nope! Try harder! *whip crack*');
     console.log('User answered ' + userTvAnswer + ', to the question "What is one of my favorited current tv shows?"');
   }
 }
 
-alert('Congratulations!! You got ' + counter + '/7 correct!');
+// Lets the user know how they did.
+
+alert('You got ' + counter + '/7 correct! That\'s how well you know me.');
 
 
 
