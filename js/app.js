e3336eab45f4185bd714e6e5cd60e99a982eb854
Adrienne <adrienneeaston@gmail.com>
2019-11-30 22:23:16 -0800

    changed vars to lets

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@
 
 // This program asks the user 7 questions about me and at teh ends provides feedback as to how many they got correct.
 
-var counter = 0;
+let counter = 0;
 
 // Question 1, yes or no question.
 
 function birthState () {
-  var born = prompt('Pop quiz about me! Did I grow up in Michigan? Yes or No?');
+  let born = prompt('Pop quiz about me! Did I grow up in Michigan? Yes or No?');
   console.log('User answered ' + born +', "for the question "Did I grow up in Michigan"');
   born = born.toLowerCase();
 
@@ -23,7 +23,7 @@ function birthState () {
 // Question 2, yes or no question.
 
 function firstJob () {
-  var work = prompt('Was my first job working at Disneyland? Yes or No?');
+  let work = prompt('Was my first job working at Disneyland? Yes or No?');
   console.log('User answered ' + work +', "Was my first job working at Disneyland?"');
   work = work.toLowerCase();
 
@@ -38,7 +38,7 @@ function firstJob () {
 // Question 3, yes or no question.
 
 function petType () {
-  var pets = prompt('Do I have cat? Yes or No?');
+  let pets = prompt('Do I have cat? Yes or No?');
   console.log('User answered ' + pets +', "Do I have a cat?"');
   pets = pets.toLowerCase();
 
@@ -53,7 +53,7 @@ function petType () {
 // Question 4, yes or no question.
 
 function favoriteBand () {
-  var music = prompt('Is Pearl Jam my favorite band? Yes or No?');
+  let music = prompt('Is Pearl Jam my favorite band? Yes or No?');
   console.log('User answered ' + music +', "Is Pearl Jam my favorite band?"');
   music = music.toLowerCase();
 
@@ -68,7 +68,7 @@ function favoriteBand () {
 // Question 5, yes or no question.
 
 function favoriteCandy () {
-  var candy = prompt('Is Turkish Delight my favorite candy? Yes or No?');
+  let candy = prompt('Is Turkish Delight my favorite candy? Yes or No?');
   console.log('User answered ' + candy +', "Is turkish Delight my favorite candy?"');
   candy = candy.toLowerCase();
 
@@ -84,9 +84,9 @@ function favoriteCandy () {
 // Error in this question is that it currently views non-numbers as correct answers. When I tried fixing it by ending in the wrong answer for "else", it broke the code, so I switched it back for now.
 
 function numberBurgers () {
-  for (var i = 0; i < 4; i++) {
+  for (let i = 0; i < 4; i++) {
 
-    var number = prompt('How many McDonald\'s hamburgers have I eaten in my life?');
+    let number = prompt('How many McDonald\'s hamburgers have I eaten in my life?');
     console.log('User believes I had ' + number + ' "McDonald\'s hamburgers in my life"');
 
     if (number > 0) {
@@ -106,16 +106,16 @@ function numberBurgers () {
 // Question 7, asks for an answer with multiple possible correct answers stored in an array. The loop stops after they have either gotten it right or have tried 6 times.
 
 function favoriteShows () {
-  for (var u = 0; u < 6; u++){
+  for (let u = 0; u < 6; u++){
 
-    var televisionShows = [
+    let televisionShows = [
       'the good place',
       'game of thrones',
       'steven universe',
       'bojack horseman'
     ];
 
-    var userTvAnswer = prompt('What is one of my favorite current tv shows?');
+    let userTvAnswer = prompt('What is one of my favorite current tv shows?');
     userTvAnswer = userTvAnswer.toLowerCase();
     if(televisionShows.includes(userTvAnswer)) {
       counter++;
